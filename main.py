f974a970e1160f72a76f95a9c58c5d48a99c175f
Sonja Friedl <72401504+Soleviso@users.noreply.github.com>
2020-11-30 17:01:16 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -6,13 +6,17 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route("/styles")
-def styles():
-    return render_template("styles.html")
 
 @app.route("/produkte")
 def produkte():
     return render_template("produkte.html")
+
+
+@app.route("/styles")
+def styles():
+    return render_template("styles.html")
+
+
 
 if __name__ == "__main__":
     app.run()
